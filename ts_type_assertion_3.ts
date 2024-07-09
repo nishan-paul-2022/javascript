32ae4160c6bd1c9a7d3f143929cc3ca5084e7230
nishan-paul-2022 <nishan.paul.hello@gmail.com>
2024-07-10 04:51:51 +0600

    ts type assertion 03

---
+++ ts_type_assertion_3.ts
@@ -0,0 +1,31 @@
+class Class1 {
+	method1(): void {
+		console.log('method-1 / class-1');
+	}
+}
+
+class Class2 extends Class1 {
+	method1(): void {
+		console.log('method-1 / class-2');
+	}
+
+	method2(): void {
+		console.log('method-2 / class-2');
+	}
+}
+
+const inst1 = new Class1();
+const inst2 = new Class2();
+const inst12 = inst1 as Class2;
+const inst21 = inst2 as Class1;
+
+inst1.method1();
+
+inst2.method1();
+inst2.method2();
+
+inst12.method1();
+// inst12.method2(); /* invalid */
+
+inst21.method1();
+// inst21.method2(); /* invalid */
