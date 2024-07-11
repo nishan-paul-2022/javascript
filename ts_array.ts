d71bfacb5ccecbf7ca655a77bfd92cb6dae9030b
nishan-paul-2022 <nishan.paul.hello@gmail.com>
2024-07-11 09:55:51 +0600

    ts array (update)

--- ts_array.ts
+++ ts_array.ts
@@ -1,43 +1,50 @@
 function F01() {
-	const arr: number[] = [1];
-	// arr.push('a');
-	arr.push(2);
+	const arr: number[] = [120];
+	arr.push(220);
+	// arr.push('ubuntu');
+	// arr.push(true);
 	console.log(arr);
 }
 
 function F02() {
-	const arr: (number | string)[] = [1, 'a'];
-	arr.push('b');
-	arr.push(2);
+	const arr: (number | string)[] = [120, 'paul'];
+	arr.push(220);
+	arr.push('ubuntu');
 	// arr.push(true);
 	console.log(arr);
 }
 
 function F03() {
-	const arr: [number, string] = [1, 'a']; // ensures values in initialization are in certain order
-	arr.push('b');
-	arr.push(2);
+	const arr: [number, string] = [120, 'paul']; // ensures values in initialization are in certain order
+	arr.push(220);
+	arr.push('ubuntu');
 	// arr.push(true);
 	console.log(arr);
 }
 
 function F04() {
-	const arr: readonly [number, string] = [1, 'a'];
-	// arr.push('b');
-	// arr.push(2);
+	const arr: readonly [number, string] = [120, 'paul'];
+	// arr.push(220);
+	// arr.push('ubuntu');
 	// arr.push(true);
 	console.log(arr);
 }
 
 function F05() {
+	const arr: [number, string, boolean?] = [120, 'paul'];
+	arr.push(220);
+	arr.push('ubuntu');
+	arr.push(true);
+	console.log(arr);
+}
+
+function F06() {
 	type HttpResponse = readonly [statusCode: number, statusMessage: string];
 	const response: HttpResponse = [200, 'OK'];
 	const [statusCode, statusMessage] = response;
-	const [statusCode1, statusMessage2] = response;
+	const [code, message] = response;
 	console.log(`status Code: ${statusCode}, status Message: ${statusMessage}`);
-	console.log(
-		`status Code: ${statusCode1}, status Message: ${statusMessage2}`
-	);
+	console.log(`code: ${code}, message: ${message}`);
 }
 
 F01();
@@ -45,3 +52,4 @@ F02();
 F03();
 F04();
 F05();
+F06();
