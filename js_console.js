6da29915a74745f9c96b87e2c83e845031ec1115
nishan-paul-2022 <nishan.paul.hello@gmail.com>
2024-07-13 05:43:57 +0600

    js console update

--- js_console.js
+++ js_console.js
@@ -1,37 +1,56 @@
-const error = 'error message';
-const log = 'log message';
-const warn = 'warning message';
-const arr = [1, 2, 3, 4, 5];
+function delayOperation(value) {
+	const promise = new Promise((resolve) => {
+		setTimeout(() => {
+			console.log(value);
+			resolve();
+		}, 500);
+	});
+
+	return promise;
+}
+
 const object = {
 	age: 27,
 	height: 5.5,
 	weight: 53
 };
 
-console.log(log);
-console.warn(warn);
-console.error(error);
-console.table(arr);
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const length = array.length;
+
+console.error('error message');
+
+console.log('log message');
+
+console.warn('warning message');
+
+console.table(array);
 console.table(object);
 
-console.count(object);
-console.count('object');
+console.count(array);
+console.count('ubuntu');
+console.count('ubuntu');
 console.count(Math.PI);
 console.count(Math.PI);
+console.count(Math.PI);
 
-function Delay03(value) {
-	const promise = new Promise((resolve) => {
-		setTimeout(() => {
-			console.log(value);
-			resolve();
-		}, 2000);
-	});
-
-	return promise;
+console.time('label-01');
+for (let index = 0; index < array.length; index++) {
+	const element = array[index];
+	console.log(element);
 }
+console.timeEnd('label-01');
 
-console.time('block-A');
-for (let i = 0; i < 10; i++) {
-	await Delay03(i);
+console.time('label-02');
+for (let index = 0; index < length; index++) {
+	const element = array[index];
+	console.log(element);
 }
-console.timeEnd('block-A');
+console.timeEnd('label-02');
+
+console.time('label-03');
+for (let index = 0; index < array.length; index++) {
+	const element = array[index];
+	await delayOperation(element);
+}
+console.timeEnd('label-03');
