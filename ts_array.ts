3beac8a6800c41e202ac3ec253a8aa7c540c8f36
nishan-paul-2022 <nishan.paul.hello@gmail.com>
2024-07-15 05:43:10 +0600

    ts array (update)

--- ts_array.ts
+++ ts_array.ts
@@ -1,6 +1,7 @@
 function F01() {
 	const arr: number[] = [120];
-	arr.push(220);
+	arr[0] = 220;
+	arr.push(320);
 	// arr.push('ubuntu');
 	// arr.push(true);
 	console.log(arr);
@@ -8,37 +9,64 @@ function F01() {
 
 function F02() {
 	const arr: (number | string)[] = [120, 'paul'];
-	arr.push(220);
+	arr[0] = 220;
+	arr.push(320);
 	arr.push('ubuntu');
 	// arr.push(true);
 	console.log(arr);
 }
 
 function F03() {
-	const arr: [number, string] = [120, 'paul']; // ensures values in initialization are in certain order
-	arr.push(220);
-	arr.push('ubuntu');
-	// arr.push(true);
-	console.log(arr);
+	type array = number[] | string[];
+
+	const arr1: array = ['paul', 'ubuntu'];
+	console.log(arr1);
+
+	const arr2: array = [120, 220];
+	console.log(arr2);
+
+	/*
+	const arr3: array = ['paul', 'ubuntu', 120, 220];
+	console.log(arr3); */
 }
 
 function F04() {
-	const arr: readonly [number, string] = [120, 'paul'];
-	// arr.push(220);
-	// arr.push('ubuntu');
+	const arr: [number, string] = [120, 'paul']; // ensures values in initialization are in certain order and number
+	arr[0] = 220;
+	arr.push(320);
+	arr.push('ubuntu');
 	// arr.push(true);
 	console.log(arr);
 }
 
 function F05() {
-	const arr: [number, string, boolean?] = [120, 'paul'];
-	arr.push(220);
-	arr.push('ubuntu');
-	arr.push(true);
+	const arr: [number, string][] = [[120, 'paul']];
+	arr[0][0] = 220;
+	arr[0].push(320);
+	arr[0].push('ubuntu');
+	// arr[0].push(true);
 	console.log(arr);
 }
 
 function F06() {
+	const arr: [number, string?] = [120];
+	arr[0] = 220;
+	arr.push(320);
+	arr.push('ubuntu');
+	// arr.push(true);
+	console.log(arr);
+}
+
+function F07() {
+	const arr: readonly [number, string] = [120, 'paul'];
+	// arr[0] = 220;
+	// arr.push(320);
+	// arr.push('ubuntu');
+	// arr.push(true);
+	console.log(arr);
+}
+
+function F08() {
 	type HttpResponse = readonly [statusCode: number, statusMessage: string];
 	const response: HttpResponse = [200, 'OK'];
 	const [statusCode, statusMessage] = response;
@@ -53,3 +81,5 @@ F03();
 F04();
 F05();
 F06();
+F07();
+F08();
