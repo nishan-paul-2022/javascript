ded651c14698131ed5543c7932045b331446d00d
nishan-paul-2022 <nishan.paul.hello@gmail.com>
2024-07-15 00:05:49 +0600

    ts es-module (using javascript)

---
+++ ts_es_module_javascript_defined.d.ts
@@ -0,0 +1,5 @@
+export function add(a: number, b: number): number;
+export function sub(a: number, b: number): number;
+export function mul(a: number, b: number): number;
+export function div(a: number, b: number): number;
+export default function calculate(a: number, b: number): void;
---
+++ ts_es_module_javascript_import.ts
@@ -0,0 +1,17 @@
+import calculate, {
+	add,
+	sub,
+	mul,
+	div
+} from './ts_es_module_javascript_defined.js';
+
+const addition = add(5, 3);
+const subtraction = sub(5, 3);
+const divison = div(5, 3);
+const multiplication = mul(5, 3);
+
+console.log(addition);
+console.log(subtraction);
+console.log(divison);
+console.log(multiplication);
+calculate(5, 3);
