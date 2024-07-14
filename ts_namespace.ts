f2fed6c47553ef07f80a15c138217fcc13879d18
nishan-paul-2022 <nishan.paul.hello@gmail.com>
2024-07-15 00:54:49 +0600

    ts namespace (update)

--- ts_namespace.ts
+++ ts_namespace.ts
@@ -1,10 +1,27 @@
 /* eslint-disable no-inner-declarations */
 /* eslint-disable @typescript-eslint/no-namespace */
 
+namespace Profile {
+	function random(value: number) {
+		console.log(value * Math.random());
+	}
+
+	export function showVengence(value: number) {
+		random(value);
+	}
+
+	export function isFullstackDeveloperLoading() {
+		console.log('YES');
+	}
+}
+
 enum Color {
 	Red,
-	Green,
-	Blue
+	Green
+}
+
+enum Color {
+	Blue = 3
 }
 
 namespace Color {
@@ -23,24 +40,57 @@ namespace Color {
 }
 
 namespace Color {
-	function random() {
-		console.log(Math.random());
-	}
-
 	export function isTeamLeadLoading() {
-		random();
 		console.log('YES');
 	}
 }
 
-console.log(Color);
-const value: Color = Color.Red;
-Color.showColor(value);
-Color.isTeamLeadLoading();
-// Color.random(); /* invalid */
+interface Body {
+	age: number;
+}
+
+interface Body {
+	height: number;
+}
+
+namespace Body {
+	export function showBody(value: Body) {
+		console.log(value.age, value.height);
+	}
+}
+
+namespace Body {
+	export function isDevopsLosing() {
+		console.log('YES');
+	}
+}
+
+function Usecase01() {
+	const value: number = 10;
+	console.log(Profile);
+	Profile.showVengence(value);
+	Profile.isFullstackDeveloperLoading();
+	// Profile.random(); /* invalid */
+}
+
+function Usecase02() {
+	const value: Color = Color.Red;
+	console.log(Color);
+	Color.showColor(value);
+	Color.isTeamLeadLoading();
+}
+
+function Usecase03() {
+	const value: Body = { age: 26, height: 5.5 };
+	console.log(Body);
+	Body.showBody(value);
+	Body.isDevopsLosing();
+}
+
+Usecase01();
+Usecase02();
+Usecase03();
 
 /*
-In TypeScript, namespaces support declaration merging, 
-meaning that multiple declarations with the same name will be merged into a single namespace. 
-This feature can be particularly useful when we want to extend existing namespaces or add related functionality incrementally. 
-However, using default exports with namespaces, or inside namespace is not supported. */
+- declaration merging is supported by interface, enum, namespace, enum-namespace, interface-namespace
+- using default exports with namespaces, or inside namespace is not supported. */
